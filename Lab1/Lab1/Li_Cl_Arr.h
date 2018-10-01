87af55da9f1d3ffe014ce8a1b755a26f5991ad1b
MariKush <43176370+MariKush@users.noreply.github.com>
2018-10-01 18:26:54 +0300

    testing and error correction in Array and DateTime .h

--- Lab1/Lab1/Li_Cl_Arr.h
+++ Lab1/Lab1/Li_Cl_Arr.h
@@ -2,7 +2,7 @@
 
 
 //array of whatever type
-template<typename T> class Li_Arr :{
+template<typename T> class Li_Arr {
 public:
 		//designer1
 		Li_Arr() { arr = new T[N];}
@@ -17,49 +17,59 @@ public:
 		~Li_Arr() { delete [] arr; }
 
 		//size of the array
-		int size() { return i;}
+		int size() { return kol;}
 
 		//return k(th) item
-		T return_k_el(int k) { return arr[k]; }
+		T return_k_el(int k) { 
+			if(k < kol && k>=0)return arr[k];
+			else return (T)nullptr;
+		}
 
 		//add an item to the end of the array
 		bool add_L(T d) {
-			if (i < N) return false;
-			arr[i] = d;
-			i++;
+			if (kol == N-1) return false;
+			arr[kol] = d;
+			kol++;
 			return true;
 		}
 
 		//delete all elements of the array
 		void del_all_el() {
-			while (i > 0) {
-				i--;
-				arr[i] = nullptr;
+			while (kol > 0) {
+				kol--;
+				arr[kol] = (T)nullptr;
 			}
 		}
 
 		//delete k(th) element of the array
 		void del_k_L(int k) {
-			if (k >= i) return;
-			for (int j = k; j < i; j++) {
+			if (k >= kol) return;
+			for (int j = k; j < kol; j++) {
 				arr[j] = arr[j + 1];
 			}
-			i--;
+			kol--;
 		}
 
 		//find the index of the element with the value of d
-		int find_el(T d) {
-			int k = 0;
-			while (k < i) {
-				if (d == arr[k])return k;
+		int find_first_el(T d) {
+			for (int i = 0; i < kol; i++) {
+				if (d == arr[i])return i;
 			}
 			std::cout << "not found" << std::endl;
 			return -1;
 		}
 
+		//print the entire array
+		void printArr()
+		{
+			for (int i = 0; i < kol; i++)
+				std::cout << arr[i] << "";
+			std::cout << std::endl;
+		}
+
 
 private:
 		T *arr = nullptr;
-		int i = 0; //the current number of elements in the array
+		int kol = 0; //the current number of elements in the array
 		int N = 10000; //maximum number of elements in the array
 };
--- Lab1/Lab1/main.cpp
+++ Lab1/Lab1/main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
 #include "Li_Cl_Arr.h"
 
+using std::cout;
+using std::endl;
+
 int main()
 {
+	Li_Arr <int> L;
+
+	L.printArr();
+
+	L.add_L(7);
+	L.add_L(3);
+	L.add_L(8);
+	L.add_L(0);
+	L.add_L(1);
+	L.add_L(4);
+	L.add_L(1);
+	L.add_L(8);
+
+	L.printArr();
+
+	int a = L.return_k_el(4);
+
+	cout << "a = " << a << endl;
+
+	L.del_k_L(4);
+
+	L.printArr();
+
+	int ind = L.find_first_el(0);
+
+	cout << "ind = " << ind << endl;
+
+	int kol = L.size();
+
+	cout << "kol = " << kol << endl;
+
+	L.printArr();
+
+	L.del_all_el();
+
+	L.printArr();
+
+	system("pause");
 
 	return 0;
 }
